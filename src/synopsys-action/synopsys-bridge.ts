36976ed9106fd70e8caba5dc1c5446f7cb44d802
lokesh <lokesha@synopsys.com>
2023-03-07 14:29:31 +0530

    upload artifacts

--- src/synopsys-action/synopsys-bridge.ts
+++ src/synopsys-action/synopsys-bridge.ts
@@ -54,9 +54,9 @@ export class SynopsysBridge {
     }
 
     if (osName === 'win32') {
-      this.bridgeExecutablePath = await tryGetExecutablePath(synopsysBridgePath.concat('\\bridge'), ['.exe'])
+      this.bridgeExecutablePath = await tryGetExecutablePath(synopsysBridgePath.concat('\\synopsys-bridge'), ['.exe'])
     } else {
-      this.bridgeExecutablePath = await tryGetExecutablePath(synopsysBridgePath.concat('/bridge'), [])
+      this.bridgeExecutablePath = await tryGetExecutablePath(synopsysBridgePath.concat('/synopsys-bridge'), [])
     }
 
     if (this.bridgeExecutablePath) {
@@ -179,6 +179,10 @@ export class SynopsysBridge {
       }
       if (validationErrors.length > 0) {
         error(new Error(validationErrors.join(',')))
+      }
+
+      if (inputs.INCLUDE_DIAGNOSTICS) {
+        formattedCommand.concat(SynopsysToolsParameter.SPACE).concat(SynopsysToolsParameter.DIAGNOSTICS_OPTION)
       }
 
       debug('Formatted command is - '.concat(formattedCommand))
