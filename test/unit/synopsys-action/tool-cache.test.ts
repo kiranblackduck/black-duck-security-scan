92eb724629728799c6bc50bdd0250e59908cd810
lokesh <lokesha@synopsys.com>
2023-09-11 19:59:35 +0530

    fix:  tool cache added non unlink test cases

--- test/unit/synopsys-action/tool-cache.test.ts
+++ test/unit/synopsys-action/tool-cache.test.ts
@@ -38,6 +38,90 @@ test('downloads a 35 byte file', async () => {
   expect(fs.statSync(downPath).size).toBe(35)
 })
 
+test('downloads a 35 byte file after a redirect', async () => {
+  nock('http://example.com').persist().get('/redirect-to').reply(303, undefined, {
+    location: 'http://example.com/bytes/35'
+  })
+
+  const downPath: string = await tc.downloadTool('http://example.com/redirect-to', destPath)
+
+  expect(fs.existsSync(downPath)).toBeTruthy()
+  expect(fs.statSync(downPath).size).toBe(35)
+})
+
+test('has status code in exception dictionary for HTTP error code responses', async () => {
+  nock('http://example.com').persist().get('/bytes/bad').reply(400, {
+    username: 'bad',
+    password: 'file'
+  })
+
+  expect.assertions(2)
+
+  try {
+    const errorCodeUrl = 'http://example.com/bytes/bad'
+    await tc.downloadTool(errorCodeUrl, destPath)
+  } catch (err: any) {
+    expect(err.toString()).toContain('Unexpected HTTP response: 400')
+    expect(err['httpStatusCode']).toBe(400)
+  }
+})
+
+test('works with redirect code 302', async function () {
+  nock('http://example.com').persist().get('/redirect-to').reply(302, undefined, {
+    location: 'http://example.com/bytes/35'
+  })
+
+  const downPath: string = await tc.downloadTool('http://example.com/redirect-to', destPath)
+
+  expect(fs.existsSync(downPath)).toBeTruthy()
+  expect(fs.statSync(downPath).size).toBe(35)
+})
+
+test('works with a 502 temporary failure', async function () {
+  nock('http://example.com').get('/temp502').twice().reply(502, undefined)
+  nock('http://example.com').get('/temp502').reply(200, undefined)
+
+  const statusCodeUrl = 'http://example.com/temp502'
+  await tc.downloadTool(statusCodeUrl, destPath)
+})
+
+test("doesn't retry 502s more than 3 times", async function () {
+  nock('http://example.com').get('/perm502').times(3).reply(502, undefined)
+
+  expect.assertions(1)
+
+  try {
+    const statusCodeUrl = 'http://example.com/perm502'
+    await tc.downloadTool(statusCodeUrl, destPath)
+  } catch (err: any) {
+    expect(err.toString()).toContain('502')
+  }
+})
+
+test('retries 429s', async function () {
+  nock('http://example.com').get('/too-many-requests-429').times(3).reply(429, undefined)
+  nock('http://example.com').get('/too-many-requests-429').reply(500, undefined)
+
+  try {
+    const statusCodeUrl = 'http://example.com/too-many-requests-429'
+    await tc.downloadTool(statusCodeUrl, destPath)
+  } catch (err: any) {
+    expect(err.toString()).toContain('500')
+  }
+})
+
+test("doesn't retry 404", async function () {
+  nock('http://example.com').get('/not-found-404').reply(404, undefined)
+  nock('http://example.com').get('/not-found-404').reply(500, undefined)
+
+  try {
+    const statusCodeUrl = 'http://example.com/not-found-404'
+    await tc.downloadTool(statusCodeUrl, destPath)
+  } catch (err: any) {
+    expect(err.toString()).toContain('404')
+  }
+})
+
 /**
  * Sets up a mock response body for downloadTool. This function works around a limitation with
  * nock when the response stream emits an error.
