298eb6b22d44c3f699eb726c27b290f7f82f541a
Srinath Akkem <reddyakkem@blackduck.com>
2025-03-11 19:50:56 +0530

    feat: [SIGINT-2798] Add mark_build_status support and return status code from the action.
    Added support for marking build status as ‘success', or 'failure' in the Black Duck Security Scan.
    Improved error handling and ensured the correct status code is returned from the action.
    
    Refs: [SIGINT-2798], [INTEGRATE-169]

--- src/main.ts
+++ src/main.ts
@@ -32,8 +32,8 @@ export async function run() {
     if (exitCode === 0) {
       info('Black Duck Security Action workflow execution completed.')
       isBridgeExecuted = true
-    } else if (exitCode === 8 && taskResult !== undefined && taskResult === constants.BUILD_STATUS.SUCCESS) {
-      info(`::warning::Exit Code: ${exitCode} Policy violations detected; Marking the build ${taskResult} as configured in the task input.`)
+    } else if (exitCode === 8 && taskResult === constants.BUILD_STATUS.SUCCESS) {
+      info('Marking the build ${taskResult} as configured in the task')
       isBridgeExecuted = true
     }
     return exitCode
