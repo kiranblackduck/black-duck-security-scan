25b0a9658e4e7071fb26bf831385b3d251ff4cd4
renukac <renukac@blackduck.com>
2025-06-14 15:33:42 +0530

    Updated Sarif file path check condition

--- src/blackduck-security-action/tools-parameter.ts
+++ src/blackduck-security-action/tools-parameter.ts
@@ -202,7 +202,7 @@ export class BridgeToolsParameter {
             }
           }
         }
-        if (!polarisSarifFilePath) {
+        if (polarisSarifFilePath === '') {
           polarisSarifFilePath = constants.INTEGRATION_POLARIS_DEFAULT_SARIF_FILE_PATH.trim()
         }
         polData.data.polaris.reports = {
